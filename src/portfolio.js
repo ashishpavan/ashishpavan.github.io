352799c3b1652469303918472adcb279642a6f1a
Pavan Agarwal <pragarwal2@wisc.edu>
2023-09-23 19:41:54 -0500

    Name change

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,12 +1,12 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://ashishpavan.github.io',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
+  name: 'Pavan Agarwal',
   role: 'Front End Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
