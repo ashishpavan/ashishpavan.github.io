ad700b1c9dfa33e44675902f351c5b8f23005a76
Pavan Agarwal <pragarwal2@wisc.edu>
2023-09-25 11:58:18 -0500

    header changed to PA from JS

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://ashishpavan.github.io',
-  title: 'JS.',
+  title: 'PA.',
 }
 
 const about = {
