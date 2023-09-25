d604ebc2736fd9fbe5bffdbd0242e57d410b0aa5
Pavan Agarwal <pragarwal2@wisc.edu>
2023-09-25 12:24:30 -0500

    Adds bug fixes to App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import { useContext } from 'react'
 import { ThemeContext } from './contexts/theme'
 import Header from './components/Header/Header'
 import About from './components/About/About'
-import Education from './components/Education/Education'
 import Projects from './components/Projects/Projects'
 import Skills from './components/Skills/Skills'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import Experience from './components/Experience/Experience'
-
+import Education from './components/Education/Education'
 import './App.css'
 
 const App = () => {
